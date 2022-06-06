17c3114a02e2e293fb05beee08f4cd9db3cea36d
tio ib <lucianoibarra6599@gmail.com>
2022-06-06 12:04:48 -0300

    primer commit

---
+++ Diagramador.NET/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Diagramador.NET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cargarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.menuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip.TabIndex = 2;
+            this.menuStrip.Text = "menuStrip2";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoToolStripMenuItem,
+            this.cargarToolStripMenuItem,
+            this.guardarToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(145, 26);
+            this.nuevoToolStripMenuItem.Text = "Abrir";
+            // 
+            // cargarToolStripMenuItem
+            // 
+            this.cargarToolStripMenuItem.Name = "cargarToolStripMenuItem";
+            this.cargarToolStripMenuItem.Size = new System.Drawing.Size(145, 26);
+            this.cargarToolStripMenuItem.Text = "Guardar";
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(145, 26);
+            this.guardarToolStripMenuItem.Text = "Nuevo";
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(0, 28);
+            this.splitContainer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.splitContainer.Panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Draw);
+            this.splitContainer.Panel2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.splitContainer.Size = new System.Drawing.Size(800, 422);
+            this.splitContainer.SplitterDistance = 140;
+            this.splitContainer.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer);
+            this.Controls.Add(this.menuStrip);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cargarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer;
+    }
+}
+
---
+++ Diagramador.NET/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Drawing.Drawing2D;
+
+namespace Diagramador.NET
+{
+    public partial class Form1 : Form
+    {
+        bool dibujar = false;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            panel2 = splitContainer.Panel2;
+            bmp = new Bitmap(panel2.ClientSize.Width, panel2.ClientSize.Height);
+        }
+
+
+        Bitmap bmp;
+        SplitterPanel panel2;
+        private Rectangle RcDraw;
+        private float PenWidth = 5;
+        Point actual, anterior;
+
+        
+        private void Draw(object sender, MouseEventArgs e)
+        {
+            if(dibujar)
+            {
+                anterior = e.Location;
+                panel2.Refresh();
+
+
+                panel2.CreateGraphics().DrawRectangle(
+                        preDibujo(),
+                        Math.Min(actual.X,anterior.X),
+                        Math.Min(actual.Y,anterior.Y),
+                        Math.Abs(anterior.X - actual.X),
+                        Math.Abs(anterior.Y - actual.Y)
+                    );
+                
+                
+                // Force a repaint of the region occupied by the RcDrawangle...
+
+                
+                
+
+            }
+        }
+
+        private Pen preDibujo()
+        {
+            Pen pen = new Pen(Color.Black, PenWidth);
+            pen.DashStyle = DashStyle.DashDotDot;
+            return pen;
+        }
+        
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            dibujar = true;
+
+            // Determine the initial RcDrawangle coordinates...
+            
+            actual = e.Location;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            dibujar = false;
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                Pen pen = new Pen(Color.Black, PenWidth);
+
+                g.DrawRectangle(
+                    pen,
+                    Math.Min(actual.X, anterior.X),
+                    Math.Min(actual.Y, anterior.Y),
+                    Math.Abs(anterior.X - actual.X),
+                    Math.Abs(anterior.Y - actual.Y)
+                    );
+
+                pen.Dispose();
+                g.Dispose();
+            }
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+                  // Draw the RcDrawangle...
+                    
+        }
+
+       
+    }
+}
